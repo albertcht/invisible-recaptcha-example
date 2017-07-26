3a4ca4f3c6eba3ccaa9129086ba4a7f5c99d1679
Albert Chen <albert.cht@gmail.com>
2017-07-26 21:08:49 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 64px;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Invisible reCAPTCHA - Ajax Example
+                </div>
+
+                g-recaptcha-response: 
+                <div id="g-recaptcha-response">
+                    not received yet
+                </div>
+
+                {!! Form::open(['url' => '/']) !!}
+                @captcha()
+                {!! Form::submit('Sumbit') !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            _submitEvent = function() {
+                $.ajax({
+                    type: "POST",
+                    url: "/api",
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        "g-recaptcha-response": $("#g-recaptcha-response").val()
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        console.log('submit successfully');
+                        console.log(data);
+                        $('#g-recaptcha-response').html(data.token)
+                    },
+                    error: function(data) {
+                        console.log('error');
+                    }
+                });
+            };
+        </script>
+    </body>
+</html>
